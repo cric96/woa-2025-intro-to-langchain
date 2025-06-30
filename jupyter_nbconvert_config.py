e3fe40dafa553f2f2aa8bd7c7504deb0a29a3007
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2025-06-30 16:15:01 +0200

    prepare slides deployment

---
+++ jupyter_nbconvert_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+
+c.SlidesExporter.reveal_transition = 'slide'
+c.SlidesExporter.reveal_scroll = True
+c.SlidesExporter.reveal_width = "1280"
+c.SlidesExporter.reveal_height = "720"
+c.SlidesExporter.center = "true"
